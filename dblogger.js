449265523c1f60fc9c6285f3494b213b3f0a543c
Aileen Murphy <ai.murf2@gmail.com>
2019-08-17 14:00:27 -0700

    testing things out

---
+++ dblogger.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Q = require('@nmq/q/client');
+ 
+const db = new Q('database');
+ 
+db.subscribe('delete', (payload) => {
+  console.log('delete happened', payload);
+});
+ 
+db.subscribe('create', (payload) => {
+  console.log('create happened', payload);
+});
